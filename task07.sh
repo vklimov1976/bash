7107b3ff7099398315a1c7d362a4e90821508ded
Klimov Vasily <vklimov1976@gmail.com>
2020-05-11 23:19:16 +0300

    Выражение $((...)) работает в контексте "двойных ковычек", поэтому сокращаем скрипт на одну строку. Было:     while read line; do         echo "$counter: $line"         $((counter++))     done
    Стало:
        while read line; do
    	echo "$((counter++)): $line"
        done

--- task07.sh
+++ task07.sh
@@ -3,7 +3,10 @@
 exec 0<$1
 counter=1
 while read line; do
-    echo "$counter: $line"
-    $((counter++))
+    # Выражение $((...)) работает в контексте двойных ковычек, поэтому выражение 
+    # echo "$counter: $line"
+    # $((counter++))
+    # можно упростить
+    echo "$((counter++)): $line"
 done
 
